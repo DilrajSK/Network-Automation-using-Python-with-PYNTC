3609df8728fff85d2b40d03b1d2bdf673726d638
Dilraj Singh Khalsa <36675499+DilrajSK@users.noreply.github.com>
2018-03-30 21:10:51 +0530

    Add files via upload

---
+++ GetFacts_PYNTC.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import json
+from pyntc import ntc_device as NTC
+
+device = NTC(
+    host='192.168.122.3',
+    username='dilraj',
+    password='dilraj',
+    device_type='cisco_ios_ssh'
+    )
+device.open()
+
+output=device.facts
+
+print json.dumps(output, indent=4)
